0a951ee684a0eca2f053f320dc5f0859b31c10fb
Dave Quinn <dquinn8@cogeco.ca>
2021-09-15 11:47:04 -0400

    date is now displayed at top of page along with current time. every 15seconds the page will update the date/time and bg-colors of events

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,13 +3,15 @@ let dayStart = 7; //7am
 let dayEnd = 20; //8pm
 // declare an array to hold the days events
 let daysEvents = [];
+// create a global luxon date object for right now  
+let rightNow = luxon.DateTime.now();
 
 // Saves events for the day to localStorage
 const saveTheDay = function() {
         localStorage.setItem("dayPlanner", JSON.stringify(daysEvents));
     }
     // Loads events for the day from localStorage
-const loadTheDay = (function() {
+const loadTheDay = function() {
     daysEvents = JSON.parse(localStorage.getItem("dayPlanner")); // try and load the events from localStorage
 
     if (!daysEvents) { // if there are no events in localStorage
@@ -18,10 +20,24 @@ const loadTheDay = (function() {
             daysEvents[i] = ""; // set 24 blank events: one for every possible hour in the day
         }
     }
-})(); // this is immediately invoked
+};
+// this function is to be called at intervals to update the date/time and event bg-color 
+const updateTheDay = function() {
+    rightNow = luxon.DateTime.now(); //update rightNow
+
+    $("#currentTime").text(rightNow.toFormat('h:mm a')); // update time of day
+    $("#currentDay").text(rightNow.toFormat('EEEE, MMMM ') + getOrdinal(rightNow.toFormat('d'))); //update the date
+
+    jQuery('.time-block').each(function() { // go through every .time-block
+        var tbEvent = $(this).find(".discription"); //find the event discription element
+        var idx = $(this).attr("id").replace("tb-", ""); //get the hour of the event
+        addEventBgColor(tbEvent, idx); //set the bg-color
+    });
+    console.log("updated")
+};
 // Event save button was clicked
 const saveTimeBlock = function(tbDiscription, idx) {
-        //find our target element to update
+        //find our target element to update :: go to the parent: ".row" and find the first ".discription"
         let target = $(tbDiscription).closest(".row").find(".discription");
         // get the current text from textarea
         let text = target.val().trim();
@@ -39,8 +55,6 @@ const saveTimeBlock = function(tbDiscription, idx) {
     }
     // Prints out the days schedual from hour (start) to  hour (end)
 const printTimeblocks = function(start, end) {
-
-        let rightNow = luxon.DateTime.now(); // get a date object for right now
         let freshDay = rightNow.startOf('day'); // get a date object starting 12am today
 
         for (let i = dayStart; i <= dayEnd; i++) { // a loop for every hour in the day
@@ -58,7 +72,8 @@ const printTimeblocks = function(start, end) {
             let tbSave = $("<button>").addClass('col-1 saveBtn').text("SAVE");
 
             // add a background color depending if event is in past,present,future
-            addEventBgColor(tbEvent, i);
+            // this is now handeled in updateDay which runs periodically
+            // addEventBgColor(tbEvent, i);
 
             //compile all elements
             tb.append($("<div>").addClass('row').append(tbTime, tbEvent, tbSave));
@@ -66,6 +81,41 @@ const printTimeblocks = function(start, end) {
             //add to timeblock list
             $("#Timeblocks").append(tb);
         }
+    }
+    //this function takes in an element and an hour of the day. it will set the element
+    // "tbEvent" background color by comparing the hour given to the current hour of the day
+const addEventBgColor = function(tbEvent, eventTime) {
+        let rightNow = luxon.DateTime.now(); // get a date object for right now
+        eventTime = rightNow.startOf('day').plus({ hours: eventTime }); // create a date object with the hour set to eventTime
+
+        if (rightNow > eventTime) {
+            tbEvent.addClass("bg-secondary"); // past event
+            if (rightNow.hour === eventTime.hour) {
+                tbEvent.addClass("bg-danger"); // present event
+            }
+        } else { tbEvent.addClass("bg-success"); } // future event
+    }
+    // Because luxion does not support ordinals this helper function is needed
+    // it uses Intl.PluralRules API built into modern browsers
+    // the following code wrapped in the getOrdinal function is taken directly from
+    // https://v8.dev/features/intl-pluralrules
+const getOrdinal = function(n) {
+        const pr = new Intl.PluralRules('en-US', {
+            type: 'ordinal'
+        });
+        const suffixes = new Map([
+            ['one', 'st'],
+            ['two', 'nd'],
+            ['few', 'rd'],
+            ['other', 'th'],
+        ]);
+        const formatOrdinals = (n) => {
+            const rule = pr.select(n);
+            const suffix = suffixes.get(rule);
+            return `${n}${suffix}`;
+        };
+
+        return formatOrdinals(n);
     }
     // Event text was clicked to be edited
 $("#Timeblocks").on("click", "p.discription", function() {
@@ -75,7 +125,6 @@ $("#Timeblocks").on("click", "p.discription", function() {
     $(this).replaceWith(textInput);
     textInput.trigger("focus");
 });
-
 // Event text being edited lost focus
 $("#Timeblocks").on("blur", "textarea", function(event) {
 
@@ -97,20 +146,10 @@ $("#Timeblocks").on("blur", "textarea", function(event) {
     }
 });
 
-const addEventBgColor = function(tbEvent, eventTime) {
-    let rightNow = luxon.DateTime.now(); // get a date object for right now
-    eventTime = rightNow.startOf('day').plus({ hours: eventTime }); // create a date object with the eventTime
+const startTheDay = (() => {
+    loadTheDay();
+    printTimeblocks(dayStart, dayEnd);
+    updateTheDay();
 
-    if (rightNow > eventTime) {
-        tbEvent.addClass("bg-secondary"); // past event
-
-        if (rightNow.hour <= eventTime.hour) {
-            tbEvent.addClass("bg-danger"); // present event
-        }
-    } else { tbEvent.addClass("bg-success"); } // future event
-
-
-}
-
-//loadTheDay();
-printTimeblocks(dayStart, dayEnd);
+    setInterval(updateTheDay, 15000);
+})();
